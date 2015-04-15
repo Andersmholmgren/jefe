4a27e696fc7b54368db703265dd2340fb956b020
Anders Holmgren <andersmholmgren@gmail.com>
2015-04-16 04:48:51 +1000

    sketching out separating commands from the core model

--- lib/src/project_operations/git_feature.dart
+++ lib/src/project_operations/git_feature.dart
@@ -1,8 +1,13 @@
 library devops.project.operations.git.feature;
 
 import 'dart:async';
+import 'package:devops/src/project.dart';
+import 'impl/git_feature_impl.dart';
+
+typedef GitFeatureCommands GitFeatureCommandsFactory(ProjectSource source);
 
 abstract class GitFeatureCommands {
+  factory GitFeatureCommands(ProjectSource source) = GitFeatureCommandsFlowImpl;
   Future init();
   Future featureStart(String featureName);
   Future featureFinish(String featureName);
--- lib/src/project_operations/project_lifecycle.dart
+++ lib/src/project_operations/project_lifecycle.dart
@@ -2,8 +2,14 @@ library devops.project.operations.git;
 
 import 'dart:async';
 import 'package:devops/src/project.dart';
+import 'package:devops/src/project_operations/git_feature.dart';
 
 abstract class ProjectLifecycle {
+  factory ProjectLifecycle(ProjectSource source,
+      {GitFeatureCommandsFactory gitFeatureFactory}) {
+    throw new StateError('Not implemented yet');
+  }
+
   Future startNewFeature(String featureName,
       {bool doPush: false, bool recursive: true});
 
