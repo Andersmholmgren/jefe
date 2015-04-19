0cdb1f6374318e98a56fddbbc74344acdf5b7787
Anders Holmgren <andersmholmgren@gmail.com>
2015-04-19 18:17:15 +1000

    jefe release

--- bin/jefe.dart
+++ bin/jefe.dart
@@ -58,6 +58,14 @@ class Jefe {
     await executor.execute(lifecycle.completeFeature(featureName));
   }
 
+  @SubCommand(help: 'Create a release of all the projects')
+  release(String featureName, {@Option(
+      help: 'The directory that contains the root of the projecs',
+      abbr: 'd') String rootDirectory: '.'}) async {
+    final executor = await load(rootDirectory);
+    await executor.executeAll(lifecycle.release());
+  }
+
   @SubCommand(help: 'Runs the given command in all projects')
   exec(String command, @Rest() List<String> args, {@Option(
       help: 'The directory that contains the root of the projecs',
