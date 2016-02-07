e72cd56a54bebe6185939fc4cca5af92d026dd59
Anders Holmgren <andersmholmgren@gmail.com>
2016-02-07 13:33:11 +1100

    JefeProject

--- lib/src/project/impl/jefe_project_impl.dart
+++ lib/src/project/impl/jefe_project_impl.dart
@@ -9,6 +9,7 @@ import 'package:jefe/src/project/impl/project_impl.dart';
 import 'package:jefe/src/project/jefe_project.dart';
 import 'package:logging/logging.dart';
 import 'package:pubspec/pubspec.dart';
+import 'dart:async';
 
 Logger _log = new Logger('jefe.project.jefe.impl');
 
@@ -26,4 +27,34 @@ class JefeProjectImpl extends ProjectImpl implements JefeProject {
   JefeProjectImpl(this.directDependencies, String gitUri,
       Directory installDirectory, PubSpec pubspec)
       : super(gitUri, installDirectory, pubspec);
+
+  Iterable<JefeProject> get depthFirst {
+    Set<JefeProject> visited = new Set();
+    return _rootNodes.expand((n) => n.getDepthFirst(visited));
+  }
+
+//  /// The [JefeProject] for the given [projectName]
+//  JefeProject forProject(String projectName) =>
+//    depthFirst.firstWhere((pd) => pd.name == projectName);
+
+  /// Iterates over [depthFirst] invoking process for each
+  Future processDepthFirst(
+      process(JefeProject project, Iterable<JefeProject> dependencies)) async {
+    await Future.forEach(
+        depthFirst, (JefeProject pd) => process(pd, pd.directDependencies));
+  }
+
+//  Iterable<JefeProject> getDepthFirst(Set<JefeProject> visited) {
+//    final children = _dependencies.expand((n) => n.getDepthFirst(visited));
+//
+//    Iterable us() sync* {
+//      if (!visited.contains((project))) {
+//        visited.add(project);
+//        yield this;
+//      }
+//    }
+//
+//    return concat([children, us()]);
+//  }
+
 }
