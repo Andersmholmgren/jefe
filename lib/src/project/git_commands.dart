97185f56e145c8810ee7378ee2f0848940ccf2b9
Anders Holmgren <andersmholmgren@gmail.com>
2016-02-12 18:39:45 +1100

    JefeProject

---
+++ lib/src/project/git_commands.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2015, Anders Holmgren. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library jefe.project.commands.git;
+
+import 'dart:async';
+
+abstract class GitCommands {
+  Future commit(String message);
+
+  Future push();
+
+  Future fetch();
+
+  Future merge(String commit);
+
+  Future updateFromRemote(String branchName, [String remoteName = 'origin']);
+
+  Future assertWorkingTreeClean();
+
+  Future assertOnBranch(String branchName);
+
+  Future checkout(String branchName);
+}
