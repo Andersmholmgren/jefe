0d1554af5b6d01dd0ce7374016b4eead0ab7f675
Anders Holmgren <andersmholmgren@gmail.com>
2016-02-14 13:13:25 +1100

    JefeProject

--- lib/src/project/impl/BaseCommandsImpl.dart
+++ lib/src/project/impl/BaseCommandsImpl.dart
@@ -1,22 +1,67 @@
 import 'package:jefe/src/project/jefe_project.dart';
 import 'dart:async';
 import 'package:jefe/src/project_commands/project_command.dart'
-    show executeTask;
+    show Callable, CommandConcurrencyMode, executeTask;
 
-abstract class BaseCommandsImpl {
+typedef Future<S> SingleProjectCommandFactory<S>(JefeProject project);
+typedef Future<T> SingleProjectCommand<S, T>(S single);
+typedef Callable<T> _Processor<T>(
+    String taskDescription, ProjectFunction/*<T>*/ command,
+    {ProjectFilter filter, Combiner/*<T>*/ combine});
+
+abstract class BaseCommandsImpl<S> {
   final JefeProjectGraph graph;
+  final SingleProjectCommandFactory<S> _singleProjectCommandFactory;
 
-  BaseCommandsImpl(this.graph);
+  BaseCommandsImpl(this.graph, this._singleProjectCommandFactory);
 
-  /// Iterates over [depthFirst] invoking [command] for each
+  Future/*<T>*/ processAllConcurrently2/*<T>*/(
+      String taskDescription, SingleProjectCommand<S, dynamic/*=T*/ > command,
+      {ProjectFilter filter, Combiner/*<T>*/ combine}) {
+    Future/*<T>*/ _executeOnSingleProject(JefeProject p) async {
+      return command(await _singleProjectCommandFactory(p));
+    }
+
+    return graph.processAllConcurrently(_executeOnSingleProject,
+        filter: filter, combine: combine);
+  }
+
   Future/*<T>*/ processDepthFirst/*<T>*/(ProjectFunction/*<T>*/ command,
           {ProjectFilter filter, Combiner/*<T>*/ combine}) =>
       graph.processDepthFirst(command, filter: filter, combine: combine);
 
-  /// Invokes [command] on this project and all reachable dependencies.
-  /// [command] is executed concurrently on all projects.
-  /// An optional [filter] can be provided to limit which projects the [command]
-  /// is executed on.
+  Callable/*<T>*/ _concurrentProcessor/*<T>*/(
+      String taskDescription, ProjectFunction/*<T>*/ command,
+      {ProjectFilter filter, Combiner/*<T>*/ combine}) {
+    return () => graph.processAllConcurrently(command,
+//        (JefeProject project) =>
+//            _processOnSingeProject(project, taskDescription, command),
+        filter: filter,
+        combine: combine);
+  }
+
+  _Processor/*<T>*/ _processor(CommandConcurrencyMode mode) {
+    switch (mode) {
+      case CommandConcurrencyMode.concurrentCommand:
+        return _concurrentProcessor;
+    }
+  }
+
+  Future/*<T>*/ process/*<T>*/(
+      String taskDescription, SingleProjectCommand<S, dynamic/*=T*/ > command,
+      {ProjectFilter filter,
+      Combiner/*<T>*/ combine,
+      CommandConcurrencyMode mode: CommandConcurrencyMode.concurrentCommand}) {
+    final processor = _processor(mode);
+    executeTask(
+        taskDescription,
+        () => processor(
+            (JefeProject project) =>
+                _processOnSingeProject2(project, taskDescription, command),
+            filter: filter,
+            combine: combine));
+  }
+
   Future/*<T>*/ processAllConcurrently/*<T>*/(
           String taskDescription, ProjectFunction/*<T>*/ command,
           {ProjectFilter filter, Combiner/*<T>*/ combine}) =>
@@ -32,4 +77,22 @@ abstract class BaseCommandsImpl {
           String taskDescription, ProjectFunction/*<T>*/ command) =>
       executeTask('$taskDescription for project ${project.name}',
           () => command(project));
+
+  Future/*<T>*/ processAllConcurrently2/*<T>*/(String taskDescription,
+          SingleProjectCommand<S, dynamic/*=T*/ > command,
+          {ProjectFilter filter, Combiner/*<T>*/ combine}) =>
+      executeTask(
+          taskDescription,
+          () => graph.processAllConcurrently(
+              (JefeProject project) =>
+                  _processOnSingeProject2(project, taskDescription, command),
+              filter: filter,
+              combine: combine));
+
+  Future/*<T>*/ _processOnSingeProject2/*<T>*/(
+          JefeProject project,
+          String taskDescription,
+          SingleProjectCommand<S, dynamic/*=T*/ > command) =>
+      executeTask('$taskDescription for project ${project.name}',
+          () async => command(await _singleProjectCommandFactory(project)));
 }
--- lib/src/project/impl/git_commands_impl.dart
+++ lib/src/project/impl/git_commands_impl.dart
@@ -13,30 +13,38 @@ import 'package:jefe/src/project_commands/project_command.dart';
 import 'package:logging/logging.dart';
 import 'package:option/option.dart';
 import 'package:pub_semver/pub_semver.dart';
+import 'package:git/git.dart';
 
 Logger _log = new Logger('jefe.project.commands.git.impl');
 
-class GitCommandsImpl extends BaseCommandsImpl implements GitCommands {
-  GitCommandsImpl(JefeProjectGraph graph) : super(graph);
+class GitCommandsImpl extends BaseCommandsImpl<GitCommands> implements GitCommands {
+  GitCommandsImpl(JefeProjectGraph graph) : super(graph,
+    (JefeProject p) async => new _GitCommandsImpl(p, await p.gitDir));
 
-//  Future<GitDir> get _gitDir => _graph.gitDir;
+//  Future<_GitCommandsImpl> _single(JefeProject p) async =>
+//    new _GitCommandsImpl(p, await p.gitDir);
+//
+//  typedef Future/*<T>*/ X/*<T>*/(_GitCommandsImpl single);
+
+  Future com(String message) => single((GitCommands s) => s.commit(message));
 
   @override
   Future commit(String message) => processAllConcurrently('git commit',
-      (JefeProject p) async => gitCommit(await p.gitDir, message));
+      (JefeProject p) async => await _single(p).commit(message));
 
   @override
-  Future push() => executeTask('git push', () async => gitPush(await _gitDir));
+  Future push() => processAllConcurrently(
+      'git push', (JefeProject p) async => gitPush(await p.gitDir));
 
   @override
-  Future fetch() => executeTask('git fetch', () async {
-        await gitFetch(await _gitDir);
+  Future fetch() => executeTask('git fetch', (JefeProject p) async {
+        await gitFetch(await p.gitDir);
       });
 
   @override
   Future assertWorkingTreeClean() =>
-      executeTask('git assertWorkingTreeClean', () async {
-        if (!await (await _gitDir).isWorkingTreeClean()) {
+      executeTask('git assertWorkingTreeClean', (JefeProject p) async {
+        if (!await (await p.gitDir).isWorkingTreeClean()) {
           throw new StateError(
               'working directory dirty for project ${graph.name}');
         }
@@ -44,9 +52,9 @@ class GitCommandsImpl extends BaseCommandsImpl implements GitCommands {
 
   @override
   Future assertOnBranch(String branchName) =>
-      executeTask('git assertOnBranch $branchName', () async {
+      executeTask('git assertOnBranch $branchName', (JefeProject p) async {
         var currentBranchName =
-            (await (await _gitDir).getCurrentBranch()).branchName;
+            (await (await p.gitDir).getCurrentBranch()).branchName;
         if (currentBranchName != branchName) {
           throw new StateError(
               '${graph.name} is on different branch ($currentBranchName) than '
@@ -56,26 +64,95 @@ class GitCommandsImpl extends BaseCommandsImpl implements GitCommands {
 
   @override
   Future checkout(String branchName) =>
-      executeTask('git checkout $branchName', () async {
-        await gitCheckout(await _gitDir, branchName);
+      executeTask('git checkout $branchName', (JefeProject p) async {
+        await gitCheckout(await p.gitDir, branchName);
       });
 
   @override
   Future updateFromRemote(String branchName, [String remoteName = 'origin']) =>
-      executeTask('git update from remote: $branchName', () async {
-        final gitDir = await _gitDir;
+      executeTask('git update from remote: $branchName', (JefeProject p) async {
+        final gitDir = await p.gitDir;
         await gitCheckout(gitDir, branchName);
         await gitMerge(gitDir, '$remoteName/$branchName');
       });
 
   @override
-  Future merge(String commit) => executeTask('git merge $commit', () async {
-        await gitMerge(await _gitDir, commit);
+  Future merge(String commit) =>
+      executeTask('git merge $commit', (JefeProject p) async {
+        await gitMerge(await p.gitDir, commit);
       });
 
   @override
   Future<bool> hasChangesSince(Version sinceVersion) async {
-    return (await diffSummarySince(await _gitDir, sinceVersion.toString()))
+    return (await diffSummarySince(await p.gitDir, sinceVersion.toString()))
         is Some;
+  }
+}
+
+
+class _GitCommandsImpl implements GitCommands {
+  final JefeProject _project;
+  final GitDir _gitDir;
+
+  _GitCommandsImpl(this._project, this._gitDir);
+
+//  Future<GitDir> get _gitDir => _graph.gitDir;
+
+  @override
+  Future commit(String message) => gitCommit(_gitDir, message);
+
+  @override
+  Future push() => gitPush(_gitDir);
+
+  @override
+  Future fetch() => executeTask('git fetch', (JefeProject p) async {
+    await gitFetch(await p.gitDir);
+  });
+
+  @override
+  Future assertWorkingTreeClean() =>
+    executeTask('git assertWorkingTreeClean', (JefeProject p) async {
+      if (!await (await p.gitDir).isWorkingTreeClean()) {
+        throw new StateError(
+          'working directory dirty for project ${graph.name}');
+      }
+    });
+
+  @override
+  Future assertOnBranch(String branchName) =>
+    executeTask('git assertOnBranch $branchName', (JefeProject p) async {
+      var currentBranchName =
+        (await (await p.gitDir).getCurrentBranch()).branchName;
+      if (currentBranchName != branchName) {
+        throw new StateError(
+          '${graph.name} is on different branch ($currentBranchName) than '
+            'expected ($branchName). Make sure you run jefe finish first');
+      }
+    });
+
+  @override
+  Future checkout(String branchName) =>
+    executeTask('git checkout $branchName', (JefeProject p) async {
+      await gitCheckout(await p.gitDir, branchName);
+    });
+
+  @override
+  Future updateFromRemote(String branchName, [String remoteName = 'origin']) =>
+    executeTask('git update from remote: $branchName', (JefeProject p) async {
+      final gitDir = await p.gitDir;
+      await gitCheckout(gitDir, branchName);
+      await gitMerge(gitDir, '$remoteName/$branchName');
+    });
+
+  @override
+  Future merge(String commit) =>
+    executeTask('git merge $commit', (JefeProject p) async {
+      await gitMerge(await p.gitDir, commit);
+    });
+
+  @override
+  Future<bool> hasChangesSince(Version sinceVersion) async {
+    return (await diffSummarySince(await p.gitDir, sinceVersion.toString()))
+    is Some;
   }
 }
