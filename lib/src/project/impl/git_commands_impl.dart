d7a26f359098b6bb18043a698422e431c56b4f64
Anders Holmgren <andersmholmgren@gmail.com>
2016-02-14 13:36:18 +1100

    JefeProject

--- lib/src/project/impl/git_commands_impl.dart
+++ lib/src/project/impl/git_commands_impl.dart
@@ -17,24 +17,78 @@ import 'package:git/git.dart';
 
 Logger _log = new Logger('jefe.project.commands.git.impl');
 
-class GitCommandsImpl extends BaseCommandsImpl<GitCommands> implements GitCommands {
-  GitCommandsImpl(JefeProjectGraph graph) : super(graph,
-    (JefeProject p) async => new _GitCommandsImpl(p, await p.gitDir));
+class GitCommandsImpl extends BaseCommandsImpl<GitCommands>
+    implements GitCommands {
+  GitCommandsImpl(JefeProjectGraph graph)
+      : super(graph,
+            (JefeProject p) async => new _GitCommandsImpl(p, await p.gitDir));
 
 //  Future<_GitCommandsImpl> _single(JefeProject p) async =>
 //    new _GitCommandsImpl(p, await p.gitDir);
 //
 //  typedef Future/*<T>*/ X/*<T>*/(_GitCommandsImpl single);
 
-  Future com(String message) => single((GitCommands s) => s.commit(message));
+//  Future com(String message) => single((GitCommands s) => s.commit(message));
 
   @override
-  Future commit(String message) => processAllConcurrently('git commit',
-      (JefeProject p) async => await _single(p).commit(message));
+  Future commit(String message) =>
+      process('git commit', (GitCommands s) => s.commit(message));
 
   @override
-  Future push() => processAllConcurrently(
-      'git push', (JefeProject p) async => gitPush(await p.gitDir));
+  Future push() => process('git push', (GitCommands s) => s.push());
+
+  @override
+  Future fetch() => process('git fetch', (GitCommands s) => s.fetch());
+
+  @override
+  Future assertWorkingTreeClean() => process('git assertWorkingTreeClean',
+      (GitCommands s) => s.assertWorkingTreeClean());
+
+  @override
+  Future assertOnBranch(String branchName) => process(
+      'git assertOnBranch $branchName',
+      (GitCommands s) => s.assertOnBranch(branchName));
+
+  @override
+  Future checkout(String branchName) =>
+      executeTask('git checkout $branchName', (JefeProject p) async {
+        await gitCheckout(await p.gitDir, branchName);
+      });
+
+  @override
+  Future updateFromRemote(String branchName, [String remoteName = 'origin']) =>
+      executeTask('git update from remote: $branchName', (JefeProject p) async {
+        final gitDir = await p.gitDir;
+        await gitCheckout(gitDir, branchName);
+        await gitMerge(gitDir, '$remoteName/$branchName');
+      });
+
+  @override
+  Future merge(String commit) =>
+      executeTask('git merge $commit', (JefeProject p) async {
+        await gitMerge(await p.gitDir, commit);
+      });
+
+  @override
+  Future<bool> hasChangesSince(Version sinceVersion) async {
+    return (await diffSummarySince(await p.gitDir, sinceVersion.toString()))
+        is Some;
+  }
+}
+
+class _GitCommandsImpl implements GitCommands {
+  final JefeProject _project;
+  final GitDir _gitDir;
+
+  _GitCommandsImpl(this._project, this._gitDir);
+
+//  Future<GitDir> get _gitDir => _graph.gitDir;
+
+  @override
+  Future commit(String message) => gitCommit(_gitDir, message);
+
+  @override
+  Future push() => gitPush(_gitDir);
 
   @override
   Future fetch() => executeTask('git fetch', (JefeProject p) async {
@@ -86,73 +140,5 @@ class GitCommandsImpl extends BaseCommandsImpl<GitCommands> implements GitComman
   Future<bool> hasChangesSince(Version sinceVersion) async {
     return (await diffSummarySince(await p.gitDir, sinceVersion.toString()))
         is Some;
-  }
-}
-
-
-class _GitCommandsImpl implements GitCommands {
-  final JefeProject _project;
-  final GitDir _gitDir;
-
-  _GitCommandsImpl(this._project, this._gitDir);
-
-//  Future<GitDir> get _gitDir => _graph.gitDir;
-
-  @override
-  Future commit(String message) => gitCommit(_gitDir, message);
-
-  @override
-  Future push() => gitPush(_gitDir);
-
-  @override
-  Future fetch() => executeTask('git fetch', (JefeProject p) async {
-    await gitFetch(await p.gitDir);
-  });
-
-  @override
-  Future assertWorkingTreeClean() =>
-    executeTask('git assertWorkingTreeClean', (JefeProject p) async {
-      if (!await (await p.gitDir).isWorkingTreeClean()) {
-        throw new StateError(
-          'working directory dirty for project ${graph.name}');
-      }
-    });
-
-  @override
-  Future assertOnBranch(String branchName) =>
-    executeTask('git assertOnBranch $branchName', (JefeProject p) async {
-      var currentBranchName =
-        (await (await p.gitDir).getCurrentBranch()).branchName;
-      if (currentBranchName != branchName) {
-        throw new StateError(
-          '${graph.name} is on different branch ($currentBranchName) than '
-            'expected ($branchName). Make sure you run jefe finish first');
-      }
-    });
-
-  @override
-  Future checkout(String branchName) =>
-    executeTask('git checkout $branchName', (JefeProject p) async {
-      await gitCheckout(await p.gitDir, branchName);
-    });
-
-  @override
-  Future updateFromRemote(String branchName, [String remoteName = 'origin']) =>
-    executeTask('git update from remote: $branchName', (JefeProject p) async {
-      final gitDir = await p.gitDir;
-      await gitCheckout(gitDir, branchName);
-      await gitMerge(gitDir, '$remoteName/$branchName');
-    });
-
-  @override
-  Future merge(String commit) =>
-    executeTask('git merge $commit', (JefeProject p) async {
-      await gitMerge(await p.gitDir, commit);
-    });
-
-  @override
-  Future<bool> hasChangesSince(Version sinceVersion) async {
-    return (await diffSummarySince(await p.gitDir, sinceVersion.toString()))
-    is Some;
   }
 }
