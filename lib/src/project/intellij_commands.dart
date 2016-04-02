83172c9ba37887c6d084dd841471c2d0b9eedaf6
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-02 11:27:47 +1100

    intellij commands

---
+++ lib/src/project/intellij_commands.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2015, Anders Holmgren. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library jefe.project.commands.intellij;
+
+import 'dart:async';
+
+import 'package:jefe/src/project/jefe_project.dart';
+import 'package:quiver/check.dart';
+import 'package:xml/xml.dart';
+
+abstract class IntellijCommands extends JefeGroupCommand<IntellijCommands> {
+  Future<IntellijVcsMappings> generateGitMappings();
+}
+
+class IntellijVcsMapping {
+  final String directory;
+  final String vcs;
+
+  IntellijVcsMapping(this.directory, {this.vcs: "Git"}) {
+    checkNotNull(this.directory);
+    checkNotNull(this.vcs);
+  }
+
+  XmlNode toXml() {
+    return (new XmlBuilder()
+          ..element("mapping",
+              attributes: <String, String>{'directory': directory, 'vcs': vcs}))
+        .build();
+  }
+}
+
+class IntellijVcsMappings {
+  final Iterable<IntellijVcsMapping> vcsDirectoryMappings;
+
+  IntellijVcsMappings(this.vcsDirectoryMappings) {
+    checkNotNull(this.vcsDirectoryMappings);
+  }
+
+  XmlNode toXml() {
+    return new XmlElement(
+        new XmlName('component'),
+        <XmlAttribute>[
+          new XmlAttribute(new XmlName('name'), 'VcsDirectoryMappings')
+        ],
+        vcsDirectoryMappings.map((m) => m.toXml()));
+  }
+}
+
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="VcsDirectoryMappings">
+    <mapping directory="$PROJECT_DIR$/dockerfile" vcs="Git" />
+    <mapping directory="$PROJECT_DIR$/jefe" vcs="Git" />
+    <mapping directory="$PROJECT_DIR$/jefe_container" vcs="Git" />
+    <mapping directory="$PROJECT_DIR$/pubspec" vcs="Git" />
+    <mapping directory="$PROJECT_DIR$/stuff" vcs="Git" />
+    <mapping directory="$PROJECT_DIR$/yamlicious" vcs="Git" />
+  </component>
+</project>
+ */
