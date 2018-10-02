cdcfef707fb93d1749b5efe314cf1859bfa55586
Anders Holmgren <aholmgren@atlassian.com>
2018-10-02 15:49:21 +1000

    upgrading to dart 2

--- bin/jefe.dart
+++ bin/jefe.dart
@@ -11,10 +11,17 @@ import 'package:stack_trace/stack_trace.dart';
 
 main(List<String> arguments) {
   final runner =
-  CommandRunner('jefe', 'Manages a set of related Dart projects');
-  runner..addCommand(Install())..addCommand(Init())..addCommand(
-      Jefetise())..addCommand(Start())..addCommand(Finish())..addCommand(
-      Exec())..addCommand(SetDependencies())..addCommand(Test());
+      CommandRunner('jefe', 'Manages a set of related Dart projects');
+  runner
+    ..addCommand(Install())
+    ..addCommand(Init())
+    ..addCommand(Jefetise())
+    ..addCommand(Start())
+    ..addCommand(Finish())
+    ..addCommand(Exec())
+    ..addCommand(SetDependencies())
+    ..addCommand(Test())
+    ..addCommand(Release());
 
   Chain.capture(() {
     runner.run(arguments);
@@ -29,49 +36,43 @@ main(List<String> arguments) {
 }
 
 abstract class BaseJefeCommand extends Command {
-  addGitUri() =>
-      argParser.addOption('gitUri',
-          help: 'The git Uri containing the jefe.yaml.', abbr: 'g');
+  addGitUri() => argParser.addOption('gitUri',
+      help: 'The git Uri containing the jefe.yaml.', abbr: 'g');
 
-  addInstallDirectory() =>
-      argParser.addOption('installDirectory',
-          help: 'The directory to install into', abbr: 'd', defaultsTo: '.');
+  addInstallDirectory() => argParser.addOption('installDirectory',
+      help: 'The directory to install into', abbr: 'd', defaultsTo: '.');
 
-  addContainerDirectory() =>
-      argParser.addOption('containerDirectory',
-          help: 'The directory that contains the projects',
-          abbr: 'd',
-          defaultsTo: '.');
+  addContainerDirectory() => argParser.addOption('containerDirectory',
+      help: 'The directory that contains the projects',
+      abbr: 'd',
+      defaultsTo: '.');
 
-  addSkipCheckout() =>
-      argParser.addFlag('skipCheckout',
-          help: 'Skips the checkout of the develop branch',
-          abbr: 's',
-          defaultsTo: false);
+  addSkipCheckout() => argParser.addFlag('skipCheckout',
+      help: 'Skips the checkout of the develop branch',
+      abbr: 's',
+      defaultsTo: false);
 
-  addRootDirectory() =>
-      argParser.addOption('rootDirectory',
-          help: 'The directory that contains the root of the projecs',
-          abbr: 'd',
-          defaultsTo: '.');
+  addRootDirectory() => argParser.addOption('rootDirectory',
+      help: 'The directory that contains the root of the projecs',
+      abbr: 'd',
+      defaultsTo: '.');
 
-  addProjects() =>
-      argParser.addOption('projects',
-          help:
+  addProjects() => argParser.addOption('projects',
+      help:
           'A project name filter. Only projects whose name contains the text will run',
-          abbr: 'p');
+      abbr: 'p');
 
-  String get gitUri() => argResults['gitUri'];
+  String get gitUri => argResults['gitUri'];
 
-  String get installDirectory() => argResults['installDirectory'];
+  String get installDirectory => argResults['installDirectory'];
 
-  String get containerDirectory() => argResults['containerDirectory'];
+  String get containerDirectory => argResults['containerDirectory'];
 
-  String get rootDirectory() => argResults['rootDirectory'];
+  String get rootDirectory => argResults['rootDirectory'];
 
-  bool get skipCheckout() => argResults['skipCheckout'];
+  bool get skipCheckout => argResults['skipCheckout'];
 
-  String get projects() => argResults['projects'];
+  String get projects => argResults['projects'];
 }
 
 class Install extends BaseJefeCommand {
@@ -90,7 +91,7 @@ class Install extends BaseJefeCommand {
   Future install(String gitUri, {String installDirectory: '.'}) async {
     final Directory installDir = new Directory(installDirectory);
     final ProjectGroup projectGroup =
-    await ProjectGroup.install(installDir, gitUri);
+        await ProjectGroup.install(installDir, gitUri);
 
     await (await projectGroup.rootJefeProjects).lifecycle.init();
   }
@@ -133,12 +134,13 @@ class Init extends BaseJefeCommand {
         skipCheckout: skipCheckout);
   }
 
-  init({String gitUri,
-    String installDirectory: '.',
-    bool skipCheckout: false}) async {
+  init(
+      {String gitUri,
+      String installDirectory: '.',
+      bool skipCheckout: false}) async {
     final Directory installDir = new Directory(installDirectory);
     final ProjectGroup projectGroup =
-    await ProjectGroup.init(installDir, gitUri);
+        await ProjectGroup.init(installDir, gitUri);
 
     await (await projectGroup.rootJefeProjects)
         .lifecycle
@@ -205,7 +207,7 @@ class Exec extends BaseJefeCommand {
     addProjects();
     argParser.addFlag('executeSerially',
         help:
-        'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
+            'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
         abbr: 's',
         defaultsTo: false);
   }
@@ -223,16 +225,16 @@ class Exec extends BaseJefeCommand {
 
   exec(String command, List<String> args,
       {String rootDirectory: '.',
-        String projects,
-        bool executeSerially: false}) async {
+      String projects,
+      bool executeSerially: false}) async {
     final graph = await _loadGraph(rootDirectory);
 
     final processCommands = graph
         .multiProjectCommands(
-        projectFilter: projectNameFilter(projects),
-        defaultConcurrencyMode: executeSerially
-            ? CommandConcurrencyMode.serialDepthFirst
-            : CommandConcurrencyMode.concurrentProject)
+            projectFilter: projectNameFilter(projects),
+            defaultConcurrencyMode: executeSerially
+                ? CommandConcurrencyMode.serialDepthFirst
+                : CommandConcurrencyMode.concurrentProject)
         .processCommands;
 
     final result = await processCommands.execute(command, args);
@@ -301,203 +303,73 @@ class Test extends BaseJefeCommand {
   }
 }
 
-//class Jefe extends Command {
-//  // TODO: implement name
-//  @override
-//  String get name => null;
-//
-//  @u.Command(
-//      help: 'Manages a set of related Dart projects',
-//      plugins: const [const u.Completion()])
-//  Jefe() {
-//    Logger.root.level = Level.FINEST;
-//    Logger.root.onRecord.listen((cr) {
-//      print('${cr.time}: ${cr.message}');
-//    });
-//    hierarchicalLoggingEnabled = true;
-//  }
+class Release extends BaseJefeCommand {
+  final name = "release";
+  final description = 'Create a release of all the projects';
 
-//  @u.SubCommand(help: 'Installs a group of projects')
-//  install(
-//      @u.Positional(help: 'The git Uri containing the jefe.yaml.')
-//          String gitUri,
-//      {@u.Option(help: 'The directory to install into', abbr: 'd')
-//          String installDirectory: '.'}) async {
-//    final Directory installDir = new Directory(installDirectory);
-//    final ProjectGroup projectGroup =
-//        await ProjectGroup.install(installDir, gitUri);
-//
-//    await (await projectGroup.rootJefeProjects).lifecycle.init();
-//  }
+  Release() {
+    addRootDirectory();
+    addProjects();
+    argParser.addOption('type',
+        help: 'The type of release',
+        abbr: 't',
+        defaultsTo: ReleaseType.lowest.toString());
+    argParser.addFlag('preReleaseOnly',
+        help: 'if true then only pre release verification steps are executed',
+        defaultsTo: false);
+    argParser.addFlag('skipPreRelease',
+        help: 'if true then the pre release verification steps are skipped',
+        defaultsTo: false);
+    argParser.addFlag('autoUpdateHostedVersions',
+        help:
+            'if true then version numbers of hosted packages will also be updated',
+        defaultsTo: false);
+  }
 
-//  @u.SubCommand(
-//      help: 'Converts a directory of projects into a jefe group. '
-//          'WARNING renames the current directory to append _root to name')
-//  jefetise({@u.Option(help: 'The directory that contains the projects',
-//      abbr: 'd') String containerDirectory: '.'}) async {
-//    final Directory parentDir = new Directory(containerDirectory);
-//    await ProjectGroup.jefetize(parentDir);
-//  }
+  Future run() {
+    final type = _parseReleaseType(argResults['type']);
+    final preReleaseOnly = argResults['preReleaseOnly'];
+    final skipPreRelease = argResults['skipPreRelease'];
+    final autoUpdateHostedVersions = argResults['autoUpdateHostedVersions'];
+    return release(
+        type: type,
+        rootDirectory: rootDirectory,
+        projects: projects,
+        preReleaseOnly: preReleaseOnly,
+        skipPreRelease: skipPreRelease,
+        autoUpdateHostedVersions: autoUpdateHostedVersions);
+  }
 
-//  @u.SubCommand(help: 'Installs or updates a group of projects')
-//  init({@u.Option(
-//      help: 'The git Uri containing the jefe.yaml.', abbr: 'g') String gitUri,
-//    @u.Option(help: 'The directory to install into',
-//        abbr: 'd') String installDirectory: '.',
-//    @u.Flag(help: 'Skips the checkout of the develop branch',
-//        abbr: 's') bool skipCheckout: false}) async {
-//    final Directory installDir = new Directory(installDirectory);
-//    final ProjectGroup projectGroup =
-//    await ProjectGroup.init(installDir, gitUri);
-//
-////    final executor = new CommandExecutor(projectGroup);
-////    await executor.execute(lifecycle.init(doCheckout: !skipCheckout));
-//    await (await projectGroup.rootJefeProjects)
-//        .lifecycle
-//        .init(doCheckout: !skipCheckout);
-//  }
+  release(
+      {String rootDirectory: '.',
+      String projects,
+      ReleaseType type: ReleaseType.lowest,
+      bool preReleaseOnly: false,
+      bool skipPreRelease: false,
+      autoUpdateHostedVersions: false}) async {
+    final graph = await _loadGraph(rootDirectory);
 
-//  @u.SubCommand(help: 'Sets up for the start of development on a new feature')
-//  start(String featureName,
-//      {@u.Option(help: 'The directory that contains the root of the projecs',
-//          abbr: 'd') String rootDirectory: '.',
-//        @u.Option(
-//            help: 'A project name filter. Only projects whose name contains the text will run',
-//            abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    return graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .lifecycle
-//        .startNewFeature(featureName);
-//  }
-//
-//  @u.SubCommand(help: 'Completes feature and returns to development branch')
-//  finish({@u.Option(
-//      help: 'The directory that contains the root of the projects',
-//      abbr: 'd') String rootDirectory: '.',
-//    @u.Option(
-//        help: 'A project name filter. Only projects whose name contains the text will run',
-//        abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    return graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .lifecycle
-//        .completeFeature();
-//  }
-//
-//  @u.SubCommand(help: 'Runs the given command in all projects')
-//  exec(String command,
-//      @u.Rest() List<String> args,
-//      {@u.Option(help: 'The directory that contains the root of the projecs',
-//          abbr: 'd') String rootDirectory: '.',
-//        @u.Option(
-//            help: 'A project name filter. Only projects whose name contains the text will run',
-//            abbr: 'p') String projects,
-//        @u.Flag(
-//            help: 'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
-//            abbr: 's') bool executeSerially: false}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//
-//    final processCommands = graph
-//        .multiProjectCommands(
-//        projectFilter: projectNameFilter(projects),
-//        defaultConcurrencyMode: executeSerially
-//            ? CommandConcurrencyMode.serialDepthFirst
-//            : CommandConcurrencyMode.concurrentProject)
-//        .processCommands;
-//
-//    final result = await processCommands.execute(command, args);
-//    final output = result.map((r) => r.toReportString()).join('\n');
-//    stdout.write(output);
-//  }
-//
+    final lifecycle = graph
+        .multiProjectCommands(projectFilter: projectNameFilter(projects))
+        .lifecycle;
 
-//  @u.SubCommand(help: 'Set dependencies between projects')
-//  setDependencies(
-//      @u.Positional(help: 'The type of dependency to set', allowed: const [
-//        'git',
-//        'path',
-//        'hosted'
-//      ]) String type,
-//      {@u.Option(help: 'The directory that contains the root of the projecs',
-//          abbr: 'd') String rootDirectory: '.',
-//        @u.Option(
-//            help: 'A project name filter. Only projects whose name contains the text will run',
-//            abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    final pubSpec = graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .pubspecCommands;
-//
-//    switch (type) {
-//      case 'git':
-//        return pubSpec.setToGitDependencies();
-//      case 'hosted':
-//        return pubSpec.setToHostedDependencies();
-//      case 'path':
-//      default:
-//        return pubSpec.setToPathDependencies();
-//    }
-//  }
-//
-//  @u.SubCommand(help: 'Runs tests projects that have tests')
-//  test({@u.Option(help: 'The directory that contains the root of the projecs',
-//      abbr: 'd') String rootDirectory: '.',
-//    @u.Option(
-//        help: 'A project name filter. Only projects whose name contains the text will run',
-//        abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    final pub = graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .pub;
-//
-//    return pub.test();
-//  }
-//
+    // TODO: would be nice to leverage grinder here (command dependencies)
+    // somehow
 
-// ------- up to
+    if (!skipPreRelease) {
+      await lifecycle.preRelease(
+          type: type, autoUpdateHostedVersions: autoUpdateHostedVersions);
+    }
 
-//  @u.SubCommand(help: 'Create a release of all the projects')
-//  release({@u.Option(
-//      help: 'The directory that contains the root of the projecs',
-//      abbr: 'd') String rootDirectory: '.',
-//    @u.Option(help: 'The type of release',
-//        abbr: 't',
-//        defaultsTo: ReleaseType.lowest,
-//        parser: _parseReleaseType) ReleaseType type: ReleaseType.lowest,
-//    @u.Option(
-//        help: 'A project name filter. Only projects whose name contains the text will run',
-//        abbr: 'p') String projects,
-//    @u.Flag(
-//        help: 'if true then only pre release verification steps are executed',
-//        defaultsTo: false) bool preReleaseOnly: false,
-//    @u.Flag(help: 'if true then the pre release verification steps are skipped',
-//        defaultsTo: false) bool skipPreRelease: false,
-//    @u.Flag(
-//        help: 'if true then version numbers of hosted packages will also be updated',
-//        defaultsTo: false) bool autoUpdateHostedVersions: false}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//
-//    final lifecycle = graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .lifecycle;
-//
-//    // TODO: would be nice to leverage grinder here (command dependencies)
-//    // somehow
-//
-//    if (!skipPreRelease) {
-//      await lifecycle.preRelease(
-//          type: type, autoUpdateHostedVersions: autoUpdateHostedVersions);
-//    }
-//
-//    if (!preReleaseOnly) {
-//      await lifecycle.release(
-//          type: type, autoUpdateHostedVersions: autoUpdateHostedVersions);
-//    } else {
-//      print('-------');
-//    }
-//  }
-//
+    if (!preReleaseOnly) {
+      await lifecycle.release(
+          type: type, autoUpdateHostedVersions: autoUpdateHostedVersions);
+    } else {
+      print('-------');
+    }
+  }
+}
+
 //  @u.SubCommand(help: 'Creates the Intellij vcs.xml file for project group')
 //  intellijVcs({@u.Option(
 //      help: 'The directory that contains the root of the projecs',
@@ -525,19 +397,13 @@ class Test extends BaseJefeCommand {
 //  }
 //
 
-Future<CommandExecutor> _loadExecutor(String rootDirectory) async =>
-    new CommandExecutor(await _load(rootDirectory));
-
 Future<JefeProjectGraph> _loadGraph(String rootDirectory) async =>
     await (await _load(rootDirectory)).rootJefeProjects;
 
 Future<ProjectGroup> _load(String rootDirectory) async {
   final Directory installDir =
-  rootDirectory == '.' ? Directory.current : new Directory(rootDirectory);
+      rootDirectory == '.' ? Directory.current : new Directory(rootDirectory);
   return ProjectGroup.load(installDir);
 }
 
-ReleaseType _parseReleaseType(String str) =>
-    ReleaseType
-        .fromLiteral(str)
-        .value;
+ReleaseType _parseReleaseType(String str) => ReleaseType.fromLiteral(str).value;
