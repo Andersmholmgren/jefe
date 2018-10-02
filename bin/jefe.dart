a94c2a77cf0afa00b78cb539fc4e2c488aa0eab3
Anders Holmgren <aholmgren@atlassian.com>
2018-10-02 15:38:27 +1000

    upgrading to dart 2

--- bin/jefe.dart
+++ bin/jefe.dart
@@ -4,26 +4,17 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:args/command_runner.dart';
 import 'package:jefe/jefe.dart' hide Command;
 import 'package:jefe/src/project/jefe_project.dart';
-import 'package:logging/logging.dart';
-import 'package:path/path.dart' as p;
 import 'package:stack_trace/stack_trace.dart';
-
-//import 'package:unscripted/unscripted.dart' as u;
-import 'package:args/args.dart';
-import 'package:args/command_runner.dart';
 
 main(List<String> arguments) {
   final runner =
-      CommandRunner('jefe', 'Manages a set of related Dart projects');
-  runner
-    ..addCommand(Install())
-    ..addCommand(Init())
-    ..addCommand(Jefetise())
-    ..addCommand(Start())
-    ..addCommand(Finish())
-    ..addCommand(Exec());
+  CommandRunner('jefe', 'Manages a set of related Dart projects');
+  runner..addCommand(Install())..addCommand(Init())..addCommand(
+      Jefetise())..addCommand(Start())..addCommand(Finish())..addCommand(
+      Exec())..addCommand(SetDependencies())..addCommand(Test());
 
   Chain.capture(() {
     runner.run(arguments);
@@ -37,46 +28,84 @@ main(List<String> arguments) {
   });
 }
 
-class Install extends Command {
+abstract class BaseJefeCommand extends Command {
+  addGitUri() =>
+      argParser.addOption('gitUri',
+          help: 'The git Uri containing the jefe.yaml.', abbr: 'g');
+
+  addInstallDirectory() =>
+      argParser.addOption('installDirectory',
+          help: 'The directory to install into', abbr: 'd', defaultsTo: '.');
+
+  addContainerDirectory() =>
+      argParser.addOption('containerDirectory',
+          help: 'The directory that contains the projects',
+          abbr: 'd',
+          defaultsTo: '.');
+
+  addSkipCheckout() =>
+      argParser.addFlag('skipCheckout',
+          help: 'Skips the checkout of the develop branch',
+          abbr: 's',
+          defaultsTo: false);
+
+  addRootDirectory() =>
+      argParser.addOption('rootDirectory',
+          help: 'The directory that contains the root of the projecs',
+          abbr: 'd',
+          defaultsTo: '.');
+
+  addProjects() =>
+      argParser.addOption('projects',
+          help:
+          'A project name filter. Only projects whose name contains the text will run',
+          abbr: 'p');
+
+  String get gitUri() => argResults['gitUri'];
+
+  String get installDirectory() => argResults['installDirectory'];
+
+  String get containerDirectory() => argResults['containerDirectory'];
+
+  String get rootDirectory() => argResults['rootDirectory'];
+
+  bool get skipCheckout() => argResults['skipCheckout'];
+
+  String get projects() => argResults['projects'];
+}
+
+class Install extends BaseJefeCommand {
   final name = "install";
   final description = "Installs a group of projects";
 
   Install() {
-    argParser.addOption('gitUri',
-        help: 'The git Uri containing the jefe.yaml.', abbr: 'g');
-    argParser.addOption('installDirectory',
-        help: 'The directory to install into', abbr: 'd', defaultsTo: '.');
+    addGitUri();
+    addInstallDirectory();
   }
 
   Future run() {
-    final gitUri = argResults['gitUri'];
-    final installDirectory = argResults['installDirectory'];
     return install(gitUri, installDirectory: installDirectory);
   }
 
   Future install(String gitUri, {String installDirectory: '.'}) async {
     final Directory installDir = new Directory(installDirectory);
     final ProjectGroup projectGroup =
-        await ProjectGroup.install(installDir, gitUri);
+    await ProjectGroup.install(installDir, gitUri);
 
     await (await projectGroup.rootJefeProjects).lifecycle.init();
   }
 }
 
-class Jefetise extends Command {
+class Jefetise extends BaseJefeCommand {
   final name = "jefetise";
   final description = 'Converts a directory of projects into a jefe group. '
       'WARNING renames the current directory to append _root to name';
 
   Jefetise() {
-    argParser.addOption('containerDirectory',
-        help: 'The directory that contains the projects',
-        abbr: 'd',
-        defaultsTo: '.');
+    addContainerDirectory();
   }
 
   Future run() {
-    final containerDirectory = argResults['containerDirectory'];
     return jefetise(containerDirectory: containerDirectory);
   }
 
@@ -86,24 +115,17 @@ class Jefetise extends Command {
   }
 }
 
-class Init extends Command {
+class Init extends BaseJefeCommand {
   final name = "init";
   final description = 'Installs or updates a group of projects';
 
   Init() {
-    argParser.addOption('gitUri',
-        help: 'The git Uri containing the jefe.yaml.', abbr: 'g');
-    argParser.addOption('installDirectory',
-        help: 'The directory to install into', abbr: 'd', defaultsTo: '.');
-    argParser.addFlag('skipCheckout',
-        help: 'Skips the checkout of the develop branch',
-        abbr: 's',
-        defaultsTo: false);
+    addGitUri();
+    addInstallDirectory();
+    addSkipCheckout();
   }
 
   Future run() {
-    final gitUri = argResults['gitUri'];
-    final installDirectory = argResults['installDirectory'];
     final skipCheckout = argResults['skipCheckout'];
     return init(
         gitUri: gitUri,
@@ -111,43 +133,33 @@ class Init extends Command {
         skipCheckout: skipCheckout);
   }
 
-  init(
-      {String gitUri,
-      String installDirectory: '.',
-      bool skipCheckout: false}) async {
+  init({String gitUri,
+    String installDirectory: '.',
+    bool skipCheckout: false}) async {
     final Directory installDir = new Directory(installDirectory);
     final ProjectGroup projectGroup =
-        await ProjectGroup.init(installDir, gitUri);
+    await ProjectGroup.init(installDir, gitUri);
 
-//    final executor = new CommandExecutor(projectGroup);
-//    await executor.execute(lifecycle.init(doCheckout: !skipCheckout));
     await (await projectGroup.rootJefeProjects)
         .lifecycle
         .init(doCheckout: !skipCheckout);
   }
 }
 
-class Start extends Command {
+class Start extends BaseJefeCommand {
   final name = "start";
   final description = 'Sets up for the start of development on a new feature';
 
   Start() {
     argParser.addOption('featureName',
         help: 'The name of the feature to start', abbr: 'f');
-    argParser.addOption('rootDirectory',
-        help: 'The directory that contains the root of the projecs',
-        abbr: 'd',
-        defaultsTo: '.');
-    argParser.addOption('projects',
-        help:
-            'A project name filter. Only projects whose name contains the text will run',
-        abbr: 'p');
+
+    addRootDirectory();
+    addProjects();
   }
 
   Future run() {
     final featureName = argResults['featureName'];
-    final rootDirectory = argResults['rootDirectory'];
-    final projects = argResults['projects'];
     return start(featureName, rootDirectory: rootDirectory, projects: projects);
   }
 
@@ -161,24 +173,16 @@ class Start extends Command {
   }
 }
 
-class Finish extends Command {
+class Finish extends BaseJefeCommand {
   final name = "finish";
   final description = 'Completes feature and returns to development branch';
 
   Finish() {
-    argParser.addOption('rootDirectory',
-        help: 'The directory that contains the root of the projecs',
-        abbr: 'd',
-        defaultsTo: '.');
-    argParser.addOption('projects',
-        help:
-            'A project name filter. Only projects whose name contains the text will run',
-        abbr: 'p');
+    addRootDirectory();
+    addProjects();
   }
 
   Future run() {
-    final rootDirectory = argResults['rootDirectory'];
-    final projects = argResults['projects'];
     return finish(rootDirectory: rootDirectory, projects: projects);
   }
 
@@ -191,23 +195,17 @@ class Finish extends Command {
   }
 }
 
-class Exec extends Command {
+class Exec extends BaseJefeCommand {
   final name = "exec";
   final description = 'Runs the given command in all projects';
 
   Exec() {
     argParser.addOption('command', help: 'The command to execute', abbr: 'c');
-    argParser.addOption('rootDirectory',
-        help: 'The directory that contains the root of the projecs',
-        abbr: 'd',
-        defaultsTo: '.');
-    argParser.addOption('projects',
-        help:
-            'A project name filter. Only projects whose name contains the text will run',
-        abbr: 'p');
+    addRootDirectory();
+    addProjects();
     argParser.addFlag('executeSerially',
         help:
-            'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
+        'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
         abbr: 's',
         defaultsTo: false);
   }
@@ -215,10 +213,7 @@ class Exec extends Command {
   Future run() {
     final commandArgs = argResults.rest.toList();
     final command = argResults['command'];
-    final rootDirectory = argResults['rootDirectory'];
-    final projects = argResults['projects'];
     final executeSerially = argResults['executeSerially'];
-    final fred = 'fuck';
 
     return exec(command, commandArgs,
         rootDirectory: rootDirectory,
@@ -228,21 +223,81 @@ class Exec extends Command {
 
   exec(String command, List<String> args,
       {String rootDirectory: '.',
-      String projects,
-      bool executeSerially: false}) async {
+        String projects,
+        bool executeSerially: false}) async {
     final graph = await _loadGraph(rootDirectory);
 
     final processCommands = graph
         .multiProjectCommands(
-            projectFilter: projectNameFilter(projects),
-            defaultConcurrencyMode: executeSerially
-                ? CommandConcurrencyMode.serialDepthFirst
-                : CommandConcurrencyMode.concurrentProject)
+        projectFilter: projectNameFilter(projects),
+        defaultConcurrencyMode: executeSerially
+            ? CommandConcurrencyMode.serialDepthFirst
+            : CommandConcurrencyMode.concurrentProject)
         .processCommands;
 
     final result = await processCommands.execute(command, args);
     final output = result.map((r) => r.toReportString()).join('\n');
     stdout.write(output);
+  }
+}
+
+class SetDependencies extends BaseJefeCommand {
+  final name = "set-dependencies";
+  final description = 'Set dependencies between projects';
+
+  SetDependencies() {
+    argParser.addOption('type',
+        help: 'The type of dependency to set',
+        allowed: const ['git', 'path', 'hosted']);
+    addRootDirectory();
+    addProjects();
+  }
+
+  Future run() {
+    final type = argResults['type'];
+    return setDependencies(type,
+        rootDirectory: rootDirectory, projects: projects);
+  }
+
+  setDependencies(String type,
+      {String rootDirectory: '.', String projects}) async {
+    final graph = await _loadGraph(rootDirectory);
+    final pubSpec = graph
+        .multiProjectCommands(projectFilter: projectNameFilter(projects))
+        .pubspecCommands;
+
+    switch (type) {
+      case 'git':
+        return pubSpec.setToGitDependencies();
+      case 'hosted':
+        return pubSpec.setToHostedDependencies();
+      case 'path':
+      default:
+        return pubSpec.setToPathDependencies();
+    }
+  }
+}
+
+class Test extends BaseJefeCommand {
+  final name = "test";
+  final description = 'Runs tests projects that have tests';
+
+  Test() {
+    addRootDirectory();
+    addProjects();
+  }
+
+  Future run() {
+    return test(rootDirectory: rootDirectory, projects: projects);
+  }
+
+  test({String rootDirectory: '.', String projects}) async {
+    final graph = await _loadGraph(rootDirectory);
+    final pub = graph
+        .multiProjectCommands(projectFilter: projectNameFilter(projects))
+        .pub;
+
+    return pub.test();
   }
 }
 
@@ -330,6 +385,76 @@ class Exec extends Command {
 //        .completeFeature();
 //  }
 //
+//  @u.SubCommand(help: 'Runs the given command in all projects')
+//  exec(String command,
+//      @u.Rest() List<String> args,
+//      {@u.Option(help: 'The directory that contains the root of the projecs',
+//          abbr: 'd') String rootDirectory: '.',
+//        @u.Option(
+//            help: 'A project name filter. Only projects whose name contains the text will run',
+//            abbr: 'p') String projects,
+//        @u.Flag(
+//            help: 'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
+//            abbr: 's') bool executeSerially: false}) async {
+//    final graph = await _loadGraph(rootDirectory);
+//
+//    final processCommands = graph
+//        .multiProjectCommands(
+//        projectFilter: projectNameFilter(projects),
+//        defaultConcurrencyMode: executeSerially
+//            ? CommandConcurrencyMode.serialDepthFirst
+//            : CommandConcurrencyMode.concurrentProject)
+//        .processCommands;
+//
+//    final result = await processCommands.execute(command, args);
+//    final output = result.map((r) => r.toReportString()).join('\n');
+//    stdout.write(output);
+//  }
+//
+
+//  @u.SubCommand(help: 'Set dependencies between projects')
+//  setDependencies(
+//      @u.Positional(help: 'The type of dependency to set', allowed: const [
+//        'git',
+//        'path',
+//        'hosted'
+//      ]) String type,
+//      {@u.Option(help: 'The directory that contains the root of the projecs',
+//          abbr: 'd') String rootDirectory: '.',
+//        @u.Option(
+//            help: 'A project name filter. Only projects whose name contains the text will run',
+//            abbr: 'p') String projects}) async {
+//    final graph = await _loadGraph(rootDirectory);
+//    final pubSpec = graph
+//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
+//        .pubspecCommands;
+//
+//    switch (type) {
+//      case 'git':
+//        return pubSpec.setToGitDependencies();
+//      case 'hosted':
+//        return pubSpec.setToHostedDependencies();
+//      case 'path':
+//      default:
+//        return pubSpec.setToPathDependencies();
+//    }
+//  }
+//
+//  @u.SubCommand(help: 'Runs tests projects that have tests')
+//  test({@u.Option(help: 'The directory that contains the root of the projecs',
+//      abbr: 'd') String rootDirectory: '.',
+//    @u.Option(
+//        help: 'A project name filter. Only projects whose name contains the text will run',
+//        abbr: 'p') String projects}) async {
+//    final graph = await _loadGraph(rootDirectory);
+//    final pub = graph
+//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
+//        .pub;
+//
+//    return pub.test();
+//  }
+//
+
 // ------- up to
 
 //  @u.SubCommand(help: 'Create a release of all the projects')
@@ -373,74 +498,6 @@ class Exec extends Command {
 //    }
 //  }
 //
-//  @u.SubCommand(help: 'Runs the given command in all projects')
-//  exec(String command,
-//      @u.Rest() List<String> args,
-//      {@u.Option(help: 'The directory that contains the root of the projecs',
-//          abbr: 'd') String rootDirectory: '.',
-//        @u.Option(
-//            help: 'A project name filter. Only projects whose name contains the text will run',
-//            abbr: 'p') String projects,
-//        @u.Flag(
-//            help: 'Instead of running the commands concurrently on the projects, run only one command on one project at a time',
-//            abbr: 's') bool executeSerially: false}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//
-//    final processCommands = graph
-//        .multiProjectCommands(
-//        projectFilter: projectNameFilter(projects),
-//        defaultConcurrencyMode: executeSerially
-//            ? CommandConcurrencyMode.serialDepthFirst
-//            : CommandConcurrencyMode.concurrentProject)
-//        .processCommands;
-//
-//    final result = await processCommands.execute(command, args);
-//    final output = result.map((r) => r.toReportString()).join('\n');
-//    stdout.write(output);
-//  }
-//
-//  @u.SubCommand(help: 'Set dependencies between projects')
-//  setDependencies(
-//      @u.Positional(help: 'The type of dependency to set', allowed: const [
-//        'git',
-//        'path',
-//        'hosted'
-//      ]) String type,
-//      {@u.Option(help: 'The directory that contains the root of the projecs',
-//          abbr: 'd') String rootDirectory: '.',
-//        @u.Option(
-//            help: 'A project name filter. Only projects whose name contains the text will run',
-//            abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    final pubSpec = graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .pubspecCommands;
-//
-//    switch (type) {
-//      case 'git':
-//        return pubSpec.setToGitDependencies();
-//      case 'hosted':
-//        return pubSpec.setToHostedDependencies();
-//      case 'path':
-//      default:
-//        return pubSpec.setToPathDependencies();
-//    }
-//  }
-//
-//  @u.SubCommand(help: 'Runs tests projects that have tests')
-//  test({@u.Option(help: 'The directory that contains the root of the projecs',
-//      abbr: 'd') String rootDirectory: '.',
-//    @u.Option(
-//        help: 'A project name filter. Only projects whose name contains the text will run',
-//        abbr: 'p') String projects}) async {
-//    final graph = await _loadGraph(rootDirectory);
-//    final pub = graph
-//        .multiProjectCommands(projectFilter: projectNameFilter(projects))
-//        .pub;
-//
-//    return pub.test();
-//  }
-//
 //  @u.SubCommand(help: 'Creates the Intellij vcs.xml file for project group')
 //  intellijVcs({@u.Option(
 //      help: 'The directory that contains the root of the projecs',
@@ -476,8 +533,11 @@ Future<JefeProjectGraph> _loadGraph(String rootDirectory) async =>
 
 Future<ProjectGroup> _load(String rootDirectory) async {
   final Directory installDir =
-      rootDirectory == '.' ? Directory.current : new Directory(rootDirectory);
+  rootDirectory == '.' ? Directory.current : new Directory(rootDirectory);
   return ProjectGroup.load(installDir);
 }
 
-ReleaseType _parseReleaseType(String str) => ReleaseType.fromLiteral(str).value;
+ReleaseType _parseReleaseType(String str) =>
+    ReleaseType
+        .fromLiteral(str)
+        .value;
